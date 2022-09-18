d1570fcdfd8446131cd8fef02a48883fb8e77018
VECTOR <90543113+Rahul-AkaVector@users.noreply.github.com>
2022-09-18 13:04:29 +0530

    Add files via upload

---
+++ function_prototype.h
@@ -0,0 +1,34 @@
+//
+// Created by rahul on 09-09-2022.
+//
+
+#ifndef CRICKET_FUNCTION_PROTOTYPE_H
+#define CRICKET_FUNCTION_PROTOTYPE_H
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <ctime>
+#include <stdlib.h>
+#include <iomanip>
+#include <ctype.h>
+
+
+
+//declare functions here
+using namespace std;
+void ex();
+void intro();
+string optn();
+string pnames(string pn);
+int toss();
+int bob(int a);
+
+int player_bowling(int target);
+
+int player_batting(int target);
+
+void cricket();
+
+#endif //CRICKET_FUNCTION_PROTOTYPE_H
